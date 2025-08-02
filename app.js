1e910ec115c09ec3bd95800c2290661fccc8ae4d
sweety <sweetyksuresh@gmail.com>
2025-08-03 04:27:23 +0530

    updated after parcel

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 
-
+import React from "react";
+import ReactDOM from 'react-dom/client';
 
 
 
 const parent=React.createElement("div",{id:"parent"},React.createElement("div",{id:"child"},
-    [React.createElement("h1",{id:"head"},"heading from child"),React.createElement("h2",{id:"head2"},"I am a h2 tag")]));
+    [React.createElement("h1",{id:"head"},"heading from child"),React.createElement("h2",{id:"head2"}," I am  h4 tag")]));
 const heading=React.createElement("h1",{id:"heading"},"Hello World from react My first react program");
 const root=ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
--- readme.md
+++ readme.md
@@ -1,1 +1,20 @@
 this is react course
+
+
+#parcel
+    -dev build
+    -local serve
+    -HMR=hot module replacement
+    -parcel uses file watching alogorithm and that has written in c++
+    -parcel caches
+    -image optimization
+    -minification 
+    -bundling
+    -compress the files
+    -consisten hashing
+    -code splitting
+    -differential bundling --support to the older browsers
+    -Diagnostic
+    -Error Handling
+    -HTTPs
+    -Tree Shaking -remove unused 
